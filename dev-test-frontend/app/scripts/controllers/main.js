8db6e2ea389fdd64546555db0c35be2822173781
Hardi Bhatt <HardiBhattAtWork@gmail.com>
2015-09-09 19:01:30 -0400

    loading data with load more

--- dev-test-frontend/app/scripts/controllers/main.js
+++ dev-test-frontend/app/scripts/controllers/main.js
@@ -8,26 +8,66 @@
  * Controller of the micApp
  */
 angular.module('micApp')
-  .controller('MainCtrl', ['$scope', '$filter', 'articleService', function ($scope, filter, articleService) {
+  .controller('MainCtrl', ['$scope', '$filter', 'articleService', function ($scope, $filter, articleService) {
 
- //  	articleService.getArticles().then(function(response){
-		
-	// 	$scope.count = response.data.length;
+	  	$scope.rowCollection = [];
+	  	var maxVal = 0;
+	  	var loadMore = true;
 
- //  		$scope.rowCollection = response.data;
-	// });
+	  	loadArticleData();
 
-	$scope.loadMore = function(e) {
-		articleService.getArticles({
-            start: $scope.count,
-            desiredPosts: 2
-        }, function(data) {
-        	console.log(data);
-            if (data.length > 0) {
-                // update list
-                $scope.rowCollection = $scope.list.concat(data);
-            }
-        });
+		var pagesShown = 1;
+		var pageSize = 10;
 
-	}
+		$scope.paginationLimit = function(data) {
+	 		return pageSize * pagesShown;
+		};
+
+		$scope.hasMoreItemsToShow = function() {
+			var moreTotal = true;
+			if(loadMore == false && moreTotal == true) {
+				console.log(moreTotal);
+				var moreTotal = pagesShown < (maxVal / pageSize);
+				console.log(moreTotal);
+			}
+			var total = pagesShown < (maxVal / pageSize);
+			console.log(maxVal);
+			if (maxVal != 0 && total == false && loadMore == true){
+				loadMoreArticleData();
+				loadMore = false;
+				maxVal = 0;
+				pagesShown += 3;
+			}
+			return moreTotal;
+		};
+
+		$scope.showMoreItems = function() {
+			pagesShown = pagesShown + 1;       
+		}; 	
+
+	  	// I apply the remote data to the local scope.
+	    function applyRemoteData( newArticles ) {
+	    	maxVal = newArticles.length;
+	        $scope.rowCollection = $scope.rowCollection.concat(newArticles);
+	    }
+
+		// I load the remote data from the server.
+	    function loadArticleData() {
+	        // The friendService returns a promise.
+	        articleService.getArticles().then(
+	            function( article ) {
+	                applyRemoteData( article );
+	            }
+	        );
+	    }
+
+	    // I load the remote data from the server.
+	    function loadMoreArticleData() {
+	        // The friendService returns a promise.
+	        articleService.getMoreArticles().then(
+	            function( article ) {
+	                applyRemoteData( article );
+	            }
+	        );
+	    }
   }]);
--- dev-test-frontend/app/scripts/services/articleservice.js
+++ dev-test-frontend/app/scripts/services/articleservice.js
@@ -10,22 +10,53 @@
 angular.module('micApp')
   .service('articleService', ['$http', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-     this.getArticles = function() {
-	    var promise = $http({
+
+    // Return public API.
+    return({
+        getArticles: getArticles,
+        getMoreArticles: getMoreArticles
+    });
+
+
+    function getArticles() {
+	    var request = $http({
 	        method : 'GET',
 	        url : 'data/articles.json'
 	    }).success(function(data, status, headers, config) {
 	        return data;
 	    });   
 
-	    return promise; 
-	};
+	    return ( request.then( handleSuccess, handleError ) ); 
+	}
 		
-	this.getMoreArticles = function() {
-	    var promise = $http.get('data/more-articles.json').success(function(data, status, headers, config) {
+	function getMoreArticles() {
+	    var request = $http.get('data/more-articles.json').success(function(data, status, headers, config) {
 	        return data;
 	    });   
 
-	    return promise; 
-	};
+	    return ( request.then( handleSuccess, handleError ) );; 
+	}
+
+	// I transform the error response, unwrapping the application dta from
+    // the API response payload.
+    function handleError( response ) {
+        // The API response from the server should be returned in a
+        // nomralized format. However, if the request was not handled by the
+        // server (or what not handles properly - ex. server error), then we
+        // may have to normalize it on our end, as best we can.
+        if (
+            ! angular.isObject( response.data ) ||
+            ! response.data.message
+            ) {
+            return( $q.reject( "An unknown error occurred." ) );
+        }
+        // Otherwise, use expected error message.
+        return( $q.reject( response.data.message ) );
+    }
+    
+	// I transform the successful response, unwrapping the application data
+    // from the API response payload.
+    function handleSuccess( response ) {
+        return ( response.data );
+    }
   }]);
